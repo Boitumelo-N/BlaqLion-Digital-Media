b734f4181d794694adc235b75c2fbbaf4cb0e163
Boitumelo-N <BoitumeloNgobene@gmail.com>
2026-04-13 11:26:46 +0200

    CSS updated and the rest of the content added with JavaScript

---
+++ Javascript/script.js
@@ -0,0 +1,4 @@
+document.querySelectorAll(".video-card video").forEach(video => {
+  video.addEventListener("mouseenter", () => video.play());
+  video.addEventListener("mouseleave", () => video.pause());
+});
