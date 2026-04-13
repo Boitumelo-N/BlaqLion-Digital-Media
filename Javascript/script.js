0a590ab5e9de320233c1b64eab7490d0c62075c8
Boitumelo-N <132778186+Boitumelo-N@users.noreply.github.com>
2026-04-13 21:22:34 +0200

    update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,21 @@
-document.querySelectorAll(".video-card video").forEach(video => {
-  video.addEventListener("mouseenter", () => video.play());
-  video.addEventListener("mouseleave", () => video.pause());
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+document.querySelectorAll(".clickable").forEach((img) => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+// close when clicking X
+document.querySelector(".close").addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+// close when clicking outside image
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.style.display = "none";
+  }
 });
