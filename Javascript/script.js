07cb38741dc61868ec4180a185de0b07c60f7d41
Boitumelo-N <BoitumeloNgobene@gmail.com>
2026-04-14 11:29:38 +0200

    update Javascript and CSS

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,25 +1,15 @@
-console.log("JS is connected");
-const images = document.querySelectorAll(".clickable");
-console.log(images.length);
-
 document.addEventListener("DOMContentLoaded", function () {
   const lightbox = document.getElementById("lightbox");
   const lightboxImg = document.getElementById("lightbox-img");
 
-  document.querySelectorAll("img").forEach(img => {
-    img.addEventListener("click", function () {
+  document.querySelectorAll(".clickable").forEach(img => {
+    img.onclick = function () {
       lightbox.style.display = "flex";
       lightboxImg.src = this.src;
-    });
+    };
   });
 
-  document.querySelector(".close").addEventListener("click", function () {
+  lightbox.onclick = function () {
     lightbox.style.display = "none";
-  });
-
-  lightbox.addEventListener("click", function (e) {
-    if (e.target !== lightboxImg) {
-      lightbox.style.display = "none";
-    }
-  });
+  };
 });
